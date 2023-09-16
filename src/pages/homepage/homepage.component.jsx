c002235b19db25bc010a9c523486c4a72567e964
andrewdunetz01 <andrewdunetz01@vt.edu>
2023-09-16 11:35:48 -0400

    added hero image

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -3,12 +3,16 @@ import React from 'react';
 import './homepage.styles.scss';
 
 import { HomePageContainer } from './homepage.styles';
+import { Button } from '@mui/material';
 
 const HomePage = () => (
   <HomePageContainer>
-    <div>
-      HELLO
-    </div>
+    <section class="hero-image">
+      <h1 class="welcome-text">
+        <div>Welcome to ASLingo!</div>
+      </h1>
+      <Button className="cta-button button" variant='contained'>Start Learning!</Button>
+    </section>
   </HomePageContainer>
 );
 
